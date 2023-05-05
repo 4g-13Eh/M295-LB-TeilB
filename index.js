611c81cbefaf3e6515a054bb157d7c2928d036bf
Shahin Amon <amons@bzz.ch>
2023-05-05 10:01:57 +0000

    correction at line 59; there were 2 returns

--- index.js
+++ index.js
@@ -56,8 +56,7 @@ app.delete('/logout', (request, response) => {
     if (request.session.email){
         request.session.destroy();
         response.clearCookie();
-        return response.json({message: "amk"});
-        return response.status(204)
+        return response.status(204).json({message: "amk"});   
     }
     response.status(401)
 })
